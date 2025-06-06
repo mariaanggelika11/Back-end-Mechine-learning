d3a36c0d217a7ec57ca8e66d3236ed8bc881360b
mariaanggelika11 <aglkamaria086@gmail.com>
2025-06-06 17:42:53 +0700

    Update: tambahkan CORS dan perbaikan lainnya

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors"); // Tambahkan ini
 const routes = require("./routes");
 const { loadModel } = require("../services/loadModel");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors()); // Aktifkan CORS
 app.use(express.json());
 app.use(routes);
 
-// Tambahkan middleware error handler di bawah ini
+// Error handler
 app.use((err, req, res, next) => {
   if (err.code === "LIMIT_FILE_SIZE") {
-    // Jika file lebih dari 1MB
     return res.status(413).json({
       status: "fail",
       message: "Payload content length greater than maximum allowed: 1000000",
@@ -20,7 +21,6 @@ app.use((err, req, res, next) => {
   }
 
   if (err.message === "File must be an image") {
-    // Jika file bukan image
     return res.status(400).json({
       status: "fail",
       message: "Terjadi kesalahan dalam melakukan prediksi",
@@ -33,7 +33,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Load model dan jalankan server
 loadModel()
   .then(() => {
     app.listen(port, () => {
